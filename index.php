c840f83f07c1c8f8a0f2bf057c9f90bae9979bb7
bouhadiomar03@gmail.com <bouhadiomar03@gmail.com>
2023-04-03 15:13:24 +0000

    commiting some changes

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+    session_start();
+
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+        header("location: dashboard.php");
+        exit;
+    }
+
     header("location: login.php")
 ?>
--- logout.php
+++ logout.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
 $_SESSION = [];
 
 session_destroy();
